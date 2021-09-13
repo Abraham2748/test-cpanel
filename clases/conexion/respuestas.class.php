fb805d79f9096e479867cdfa84ab817313ec83ba
Abraham Salazar <asalazarmu@proveedor.intercorp.com.pe>
2021-09-13 12:42:31 -0500

    clase 4 - manejo de errores

---
+++ clases/conexion/respuestas.class.php
@@ -0,0 +1,38 @@
+<?php
+
+class respuestas {
+
+    private $response = [
+        "status" => "ok",
+        "result" = array()
+    ];
+
+    public function error_405() {
+        $this->response["status"] = "error";
+        $this->response["result"] = array(
+            "error_id" => "405",
+            "error_msg" => "Method Not Allowed",
+        );
+        return $response;
+    }
+    
+    public function error_200($message = "Incorrect data") {
+        $this->response["status"] = "error";
+        $this->response["result"] = array(
+            "error_id" => "200",
+            "error_msg" => $message,
+        );
+        return $response;
+    }
+
+    public function error_400() {
+        $this->response["status"] = "error";
+        $this->response["result"] = array(
+            "error_id" => "400",
+            "error_msg" => "Incomplete data",
+        );
+        return $response;
+    }
+}
+
+?>
