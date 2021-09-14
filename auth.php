ff2831c32f6dc99200482b567f898101ddde00d6
Abraham Salazar <asalazarmu@proveedor.intercorp.com.pe>
2021-09-13 23:18:03 -0500

    auth test 07

--- auth.php
+++ auth.php
@@ -1,13 +1,15 @@
 <?php
 
-require_once('clases/conexion/auth.class.php');
-require_once('clases/conexion/respuestas.class.php');
+require_once('clases/auth.class.php');
+require_once('clases/respuestas.class.php');
 
+$_auth = new auth;
 $_respuestas = new respuestas;
 
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
     $postBody = file_get_contents("php://input");
-    print_r($postBody);
+    $datosArray = $_auth->login($postBody);
+    print_r(json_encode($datosArray));
 } else {
     echo "Method not allowed";
 }
