aec6d76f51c8fc0d8be25d1dee395c01ac5adaf1
Abraham Salazar <asalazarmu@proveedor.intercorp.com.pe>
2021-09-14 22:25:18 -0500

    auth test 09

--- clases/auth.class.php
+++ clases/auth.class.php
@@ -8,9 +8,26 @@ class auth extends conexion {
         $_respuestas = new respuestas;
         $datos = json_decode($json, true);
         if(isset($datos['usuario']) && isset($datos["password"])) {
-            return $datos;
+            $usuario = $datos["usuario"];
+            $password = $datos["password"];
+            $datos = $this->obtenerDatosUsuario($usuario);
+            if($datos) {
+                return $datos;
+            } else {
+                return $_respuestas->error_200("User does not exist");
+            }
         } else {
             return $_respuestas->error_400();
+        }
+    }
+
+    private function obtenerDatosUsuario($correo) {
+        $query = "SELECT usuarioId, Password, Estado FROM users WHERE Usuario = '$correo'";
+        $datos = parent::obtenerDatos($query);
+        if(isset($datos[0]['usuarioId'])) {
+            return $datos;
+        } else {
+            return 0;
         }
     }
 }
