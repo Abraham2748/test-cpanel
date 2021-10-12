50248dee7fe11c6d6afa239dde937ead2b53875b
Abraham Salazar <asalazarmu@proveedor.intercorp.com.pe>
2021-10-11 21:42:12 -0500

    pacientes test 03

--- clases/pacientes.class.php
+++ clases/pacientes.class.php
@@ -1,20 +1,22 @@
 <?php
 
 require_once "conexion/conexion.php";
-require_once "respuestas.class.php"
+require_once "respuestas.class.php";
 
-class pacientes extends conexion {
+class pacientes extends conexion
+{
 
     private $table = "pacientes";
-    public function listaPacientes($pagina) {
+    public function listaPacientes($pagina)
+    {
         $inicio = 0;
         $cantidad = 100;
-        if($pagina > 1) {
+        if ($pagina > 1) {
             $inicio = ($cantidad * ($pagina - 1)) + 1;
             $cantidad = $cantidad * $pagina;
         }
-        
-        $query = "SELECT * FROM ". $this->table." LIMIT $inicio, $cantidad";
-        printr($query);
+
+        $query = "SELECT * FROM " . $this->table . " LIMIT $inicio, $cantidad";
+        print_r($query);
     }
 }
