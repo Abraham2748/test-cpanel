7c82631bf60e3c6b39e6d9137dcdfd673523a668
Abraham Salazar <asalazarmu@proveedor.intercorp.com.pe>
2021-10-24 21:57:46 -0500

    Spell fix 08

--- classes/auth.class.php
+++ classes/auth.class.php
@@ -8,15 +8,15 @@ class auth extends Connection
     public function login($json)
     {
         $_responses = new responses;
-        $datos = json_decode($json, true);
-        if (isset($datos['usuario']) && isset($datos["password"])) {
-            $usuario = $datos["usuario"];
-            $password = md5($datos["password"]);
-            $datosUsuario = $this->obtenerDatosUsuario($usuario);
-            if ($datosUsuario) {
-                if ($password == $datosUsuario[0]["Password"]) {
-                    if ($datosUsuario[0]["Estado"] == "Activo") {
-                        $token = $this->insertarToken($datosUsuario[0]['UsuarioId']);
+        $data = json_decode($json, true);
+        if (isset($data['username']) && isset($data["password"])) {
+            $username = $data["username"];
+            $password = md5($data["password"]);
+            $userData = $this->getUserData($username);
+            if ($userData) {
+                if ($password == $userData[0]["Password"]) {
+                    if ($userData[0]["Status"] == 1) {
+                        $token = $this->insertToken($userData[0]['Id']);
                         if ($token) {
                             $result = $_responses->response;
                             $result['result'] = array(
@@ -27,14 +27,14 @@ class auth extends Connection
                             return $_responses->error_500();
                         }
                     } else {
-                        return $_responses->error_200("User $usuario is not active.");
+                        return $_responses->error_200("User $username is not active.");
                     }
-                    return $datosUsuario;
+                    return $userData;
                 } else {
                     return $_responses->error_200("Incorrect password");
                 }
             } else {
-                return $_responses->error_200("User $usuario does not exist");
+                return $_responses->error_200("User $username does not exist");
             }
         } else {
             return $_responses->error_400();
@@ -44,10 +44,10 @@ class auth extends Connection
     public function validateToken($token)
     {
         $token = substr($token, 7);
-        $query = "SELECT * FROM UserToken WHERE Token = '" . $token . "' AND Estado = 'Activo' AND Fecha > DATE_SUB(UTC_TIMESTAMP, INTERVAL 1 HOUR)";
+        $query = "SELECT * FROM UserToken WHERE Token = '" . $token . "' AND Status = 1 AND LastDate > DATE_SUB(UTC_TIMESTAMP, INTERVAL 1 HOUR)";
         $result = parent::getData($query);
         if (sizeof($result) == 1) {
-            $query = "UPDATE UserToken SET Fecha = UTC_TIMESTAMP WHERE Token = '" . $token . "'";
+            $query = "UPDATE UserToken SET LastDate = UTC_TIMESTAMP WHERE Token = '" . $token . "'";
             parent::nonQuery($query);
             return true;
         } else {
@@ -55,25 +55,25 @@ class auth extends Connection
         }
     }
 
-    private function obtenerDatosUsuario($correo)
+    private function getUserData($username)
     {
-        $query = "SELECT * FROM User WHERE Usuario = '$correo'";
-        $datos = parent::getData($query);
-        if (isset($datos[0]['UsuarioId'])) {
-            return $datos;
+        $query = "SELECT * FROM User WHERE User = '$username'";
+        $data = parent::getData($query);
+        if (isset($data[0]['UserId'])) {
+            return $data;
         } else {
             return null;
         }
     }
 
-    private function insertarToken($usuarioId)
+    private function insertToken($userId)
     {
-        $cstrong = true;
-        $token = bin2hex(openssl_random_pseudo_bytes(16, $cstrong));
-        $estado = "Activo";
-        $query = "INSERT INTO UserToken (UsuarioId, Token, Estado, Fecha) VALUES ('$usuarioId','$token','$estado', UTC_TIMESTAMP);";
-        $existe = parent::nonQuery($query);
-        if ($existe) {
+        $cryptographicallyStrong = true;
+        $token = bin2hex(openssl_random_pseudo_bytes(16, $cryptographicallyStrong));
+        $status = 1;
+        $query = "INSERT INTO UserToken (Id_User, Token, Status, LastDate) VALUES ('$userId','$token','$status', UTC_TIMESTAMP);";
+        $exist = parent::nonQuery($query);
+        if ($exist) {
             return $token;
         } else {
             return false;
